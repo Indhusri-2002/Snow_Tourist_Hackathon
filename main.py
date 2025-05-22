da81199db1f76766ec0270e6a82b94634c042b06
Akanth24 <akanth24@gmail.com>
2025-05-22 19:50:14 +0530

    fix: changes save

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from screens import home, analytics, about
-# st.set_page_config(layout="wide", page_title="India Tourism Dashboard")
-st.set_page_config(page_title="My Streamlit App", layout="wide")
+
+st.set_page_config(page_title="India Tourism Dashboard", layout="wide")
 
 tab = st.tabs(["Home", "Analytics", "About"])
 
