b38164a7f62bc6fc5542d3914ccf53d1724c3497
Akanth24 <akanth24@gmail.com>
2025-05-24 02:20:52 +0530

    loaded data snflk

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -12,11 +12,11 @@ def load_datasets():
         df_tour_stat = pd.read_sql("SELECT * FROM STAGE.STG_TOUR_STAT", conn)
         df_monthly_fee = pd.read_sql("SELECT * FROM STAGE.STG_IND_FEE", conn)
         df_gdp_jobs = pd.read_sql("SELECT * FROM CURATE.FCT_JOBS_GDP_CAT_MIL", conn)
-        df_untouched = pd.read_sql("SELECT * FROM STAGE.STG_UNTOUCHED_PLACES", conn)
         df_amt_ftas = pd.read_sql("SELECT * FROM CURATE.FCT_AMT_FTAS", conn)
         df_artforms = pd.read_sql("SELECT * FROM STAGE.STG_ARTFORMS_OF_INDIA", conn)
         df_experiences = pd.read_sql("SELECT * FROM STAGE.STG_CULTURAL_EXPERIENCES", conn)
-
+        df_places_to_visit = pd.read_sql("SELECT * FROM STAGE.STG_PLACES_TO_VISIT", conn)
+        df_untouched_places = pd.read_sql("SELECT * FROM STAGE.STG_UNTOUCHED_PLACES_LAT_LON", conn)
     finally:
         conn.close()
 
@@ -27,9 +27,9 @@ def load_datasets():
         "scheme_amt": df_scheme_amt,
         "tour_stat": df_tour_stat,
         "monthly_fee": df_monthly_fee,
-        "untouched": df_untouched,
         "amt_ftas":df_amt_ftas,
         "art_forms":df_artforms,
         "experiences":df_experiences,
-
+        "places_to_visit": df_places_to_visit,
+        "untouched_places": df_untouched_places
     }
