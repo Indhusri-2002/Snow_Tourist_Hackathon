01f02fe77fa3b75938c3b7240df2deac61d4db5e
Indhusri-2002 <kodatiindhusri@gmail.com>
2025-05-25 00:25:12 +0530

    minor changes

--- screens/home.py
+++ screens/home.py
@@ -20,6 +20,8 @@ def render():
                 padding: 0;
             }
             .center-container {
+                margin : 0;
+                padding:0;
                 max-width: 1000px;
             }
         </style>
@@ -40,6 +42,8 @@ def render():
     df_experiences = dfs["experiences"]
     # Container
     st.markdown('<div class="center-container">', unsafe_allow_html=True)
+    st.markdown("## Explore the Art, Culture, and Heritage of India")
+
 
     # Banner
     st.markdown('<div class="full-width-img">', unsafe_allow_html=True)
@@ -47,13 +51,21 @@ def render():
     st.markdown("</div>", unsafe_allow_html=True)
 
     # Intro
-    st.markdown("### Explore the Art, Culture, and Heritage of India")
-    st.write("""
-        India is a land of diverse traditions, vibrant festivals, and rich history. 
-        From the classical dance forms of Bharatnatyam and Kathak to the architectural marvels of ancient temples 
-        and forts, every corner of India tells a unique story. Dive into the artistic soul of the country 
-        and uncover experiences that go beyond sightseeing—immerse yourself in its living heritage.
-    """)
+    st.markdown("""
+    <div style = "margin-bottom : 20px" >
+        <h1 style="font-size: 1.5rem;">Discover the Soul of India — One Culture at a Time.</h1>
+        <p style="font-size: 1rem;">
+            India is a land of diverse traditions, vibrant festivals, and rich history. 
+            From the classical dance forms of Bharatnatyam and Kathak to the architectural marvels of ancient temples 
+            and forts, every corner of India tells a unique story. Dive into the artistic soul of the country 
+            and uncover experiences that go beyond sightseeing—immerse yourself in its living heritage.
+            Uncover ancient art forms, hidden cultural gems, and stories that breathe life into every corner of the country.
+            This platform brings data-driven insights into India’s vibrant heritage and the impact of responsible tourism.
+            Join us in preserving traditions while exploring them—smartly, sustainably, and soulfully.
+        </p>
+    </div>
+""", unsafe_allow_html=True)
+
 
     # Images with Buttons
     art_image = Image.open("images/art.jpeg")
@@ -79,16 +91,22 @@ def render():
                 st.session_state.active_tab = "culture"
                 st.markdown('<script>scrollToSection("culture-section")</script>', unsafe_allow_html=True)
 
-    # Divider
-    st.markdown("---")
 
     # Render Sections
     if "active_tab" not in st.session_state:
-        st.session_state.active_tab = "art"
+        st.session_state.active_tab = ""
 
     if st.session_state.active_tab == "art":
+        st.markdown("---")
         render_artforms_section(df_artforms)
-    else:
+    elif st.session_state.active_tab == "culture":
+        st.markdown("---")
         render_experiences_section(df_experiences)
+
+    st.markdown("---")
+    st.markdown("## UNESCO identified tangible world heritage sites in India")
+    left, center, right = st.columns([2,5,2])
+    with center:
+        st.image("images/unesco_heritage.jpg","",use_container_width=True )
 
     st.markdown('</div>', unsafe_allow_html=True)
